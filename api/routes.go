02b09a7c76087a0ffdd677977256ecccab3ed186
ragtop-caddy <ragtop_caddy@protonmail.com>
2019-03-03 17:14:07 -0700

    Additional refactoring to add logging function

--- api/routes.go
+++ api/routes.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
+import "net/http"
 
 // Route - Struct to hold route information
 type Route struct {
@@ -16,46 +12,6 @@ type Route struct {
 
 // Routes - Struct to hold multiple route definitions
 type Routes []Route
-
-// NewRouter - Function called by main to create a new router
-func NewRouter() *mux.Router {
-
-	entries = append(entries, Entry{
-		SKU: "1",
-		Header: &Header{
-			Type:        "pant",
-			Description: "boys small pant",
-			Stock:       5,
-			History: []Transaction{
-				Transaction{
-					ISODate:  "1234",
-					Campus:   "duffy",
-					Students: 0,
-					Action:   "create",
-				},
-			},
-		},
-		Details: &Detail{
-			Gender: "m",
-			Color:  "blk",
-			Size:   "small",
-			Style:  "slacks",
-			Fit:    "loose",
-		},
-	},
-	)
-
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(route.HandlerFunc)
-	}
-
-	return router
-}
 
 var routes = Routes{
 	Route{
