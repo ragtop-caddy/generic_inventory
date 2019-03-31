107ccf7c72354cead87fa5f04c6c579ee3c21606
ragtop-caddy <ragtop_caddy@protonmail.com>
2019-03-31 15:00:34 -0700

    Added new handler for flexibility

--- api/controller.go
+++ api/controller.go
@@ -11,109 +11,114 @@ import (
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// GetEntries - Return a json object containing people
-func GetEntries(w http.ResponseWriter, r *http.Request) {
-	var entries []Entry
+// CrudHandle - Function to call other crud funtions
+func CrudHandle(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	switch params["action"] {
+	case "show":
+		if params["sku"] == "all" {
+			results, err := GetEntries()
+			if err != nil {
+				w.WriteHeader(404) // Not Found
+			}
+			if err := json.NewEncoder(w).Encode(results); err != nil {
+				w.WriteHeader(500) // Internal error
+			}
+		} else {
+			result, err := GetEntry(params["sku"])
+			if err != nil {
+				w.WriteHeader(404) // Not Found
+			} else {
+				if err := json.NewEncoder(w).Encode(result); err != nil {
+					w.WriteHeader(500) // Internal error
+				}
+			}
+		}
+	case "add":
+		id, err := CreateEntry(r, params["sku"])
+		if err != nil {
+			w.WriteHeader(500) // Internal error
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				w.WriteHeader(500) // Internal error
+			}
+		} else {
+			w.WriteHeader(http.StatusCreated)
+			if err := json.NewEncoder(w).Encode(id); err != nil {
+				w.WriteHeader(500) // Internal error
+			}
+		}
+	case "remove":
+		count, err := DeleteEntry(params["sku"])
+		if err != nil {
+			w.WriteHeader(500) // Internal error
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				w.WriteHeader(500) // Internal error
+			}
+		} else {
+			if count == 0 {
+				w.WriteHeader(404) // Not Found
+			}
+			if err := json.NewEncoder(w).Encode(count); err != nil {
+				w.WriteHeader(500) // Internal error
+			}
+		}
+	default:
+		if err := json.NewEncoder(w).Encode(params); err != nil {
+			w.WriteHeader(500) // Internal error
+		}
+	}
+}
+
+// GetEntries - Return a json object containing people
+func GetEntries() (results []Entry, err error) {
 	ctx, close := context.WithTimeout(context.Background(), 30*time.Second)
 	defer close()
 	c, err := InventoryDB.Collection("entries").Find(ctx, bson.D{})
-	if err != nil {
-		w.WriteHeader(404) // Not Found
-	}
 	defer c.Close(ctx)
 
 	for c.Next(ctx) {
 		var result Entry
-		err := c.Decode(&result)
-		if err != nil {
-			w.WriteHeader(500) // Internal error
-		}
-		entries = append(entries, result)
+		err = c.Decode(&result)
+		results = append(results, result)
 	}
-	if err := c.Err(); err != nil {
-		w.WriteHeader(500) // Internal error
-	}
-
-	if err := json.NewEncoder(w).Encode(entries); err != nil {
-		w.WriteHeader(500) // Internal error
-	}
+	err = c.Err()
+	return
 }
 
 // GetEntry - Return a json object containing one person
-func GetEntry(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	var params = mux.Vars(r)
-	var result Entry
-	filter := bson.M{"sku": params["sku"]}
+func GetEntry(sku string) (result Entry, err error) {
+	filter := bson.M{"sku": sku}
 	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
 	defer close()
-	err := InventoryDB.Collection("entries").FindOne(ctx, filter).Decode(&result)
-	if err != nil {
-		w.WriteHeader(404) // Not Found
-	}
-
-	if err := json.NewEncoder(w).Encode(result); err != nil {
-		w.WriteHeader(500) // Internal error
-	}
+	err = InventoryDB.Collection("entries").FindOne(ctx, filter).Decode(&result)
+	return
 }
 
 // CreateEntry - Create a json object containing one person
-func CreateEntry(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+func CreateEntry(req *http.Request, sku string) (res *mongo.InsertOneResult, err error) {
 	var entry Entry
-	params := mux.Vars(r)
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-
-	if err != nil {
-		w.WriteHeader(413) // Too Large
-	}
-
-	if err := r.Body.Close(); err != nil {
-		w.WriteHeader(500) // Internal error
-	}
-
-	if err := json.Unmarshal(body, &entry); err != nil {
-		w.WriteHeader(422) // unprocessable entity
-		if err := json.NewEncoder(w).Encode(err); err != nil {
-			w.WriteHeader(500) // Internal error
-		}
-	}
-
-	entry.SKU = params["sku"]
+	entry.SKU = sku
+	body, err := ioutil.ReadAll(io.LimitReader(req.Body, 1048576))
+	err = json.Unmarshal(body, &entry)
 	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
 	defer close()
-	res, err := InventoryDB.Collection("entries").InsertOne(ctx, entry)
-	id := res.InsertedID
-
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(id); err != nil {
-		w.WriteHeader(500) // Internal error
-	}
+	res, err = InventoryDB.Collection("entries").InsertOne(ctx, entry)
+	return
 }
 
 // DeleteEntry - Delete an entry
-func DeleteEntry(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	params := mux.Vars(r)
-	filter := bson.M{"sku": params["sku"]}
+func DeleteEntry(sku string) (count int64, err error) {
+	filter := bson.M{"sku": sku}
 	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
 	defer close()
 	res, err := InventoryDB.Collection("entries").DeleteOne(ctx, filter)
-	if err != nil {
-		w.WriteHeader(500) // Internal error
-	}
-	count := res.DeletedCount
-
-	if count == 0 {
-		w.WriteHeader(404) // Not Found
-	}
-
-	if err := json.NewEncoder(w).Encode(count); err != nil {
-		w.WriteHeader(500) // Internal error
-	}
+	count = res.DeletedCount
+	return
 }
 
 // GetIndex - Return the main HTML page for the site
--- api/routes.go
+++ api/routes.go
@@ -15,28 +15,22 @@ type Routes []Route
 
 var routes = Routes{
 	Route{
-		"All Entries",
-		"GET",
-		"/api/inventory",
-		GetEntries,
-	},
-	Route{
 		"Get Entry",
 		"GET",
-		"/api/inventory/{sku}",
-		GetEntry,
+		"/api/inventory/{action}/{sku}",
+		CrudHandle,
 	},
 	Route{
 		"Create Entry",
 		"POST",
-		"/api/inventory/{sku}",
-		CreateEntry,
+		"/api/inventory/{action}/{sku}",
+		CrudHandle,
 	},
 	Route{
 		"Delete Entry",
 		"DELETE",
-		"/api/inventory/{sku}",
-		DeleteEntry,
+		"/api/inventory/{action}/{sku}",
+		CrudHandle,
 	},
 	Route{
 		"Index",
