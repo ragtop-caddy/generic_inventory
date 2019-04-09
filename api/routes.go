7a1ce8dfe0c68750260b7052f10b8ac0e233227e
ragtop-caddy <ragtop_caddy@protonmail.com>
2019-04-08 18:58:52 -0700

    added notes for potential router config

--- api/routes.go
+++ api/routes.go
@@ -2,6 +2,34 @@ package api
 
 import "net/http"
 
+// Complex YAML for route/router config
+//type Service struct {
+//    APIVersion string `yaml:"apiVersion"`
+//    Kind       string `yaml:"kind"`
+//    Metadata   struct {
+//        Name      string `yaml:"name"`
+//        Namespace string `yaml:"namespace"`
+//        Labels    struct {
+//            RouterDeisIoRoutable string `yaml:"router.deis.io/routable"`
+//        } `yaml:"labels"`
+//        Annotations struct {
+//            RouterDeisIoDomains string `yaml:"router.deis.io/domains"`
+//        } `yaml:"annotations"`
+//    } `yaml:"metadata"`
+//    Spec struct {
+//        Type     string `yaml:"type"`
+//        Selector struct {
+//            App string `yaml:"app"`
+//        } `yaml:"selector"`
+//        Ports []struct {
+//            Name       string `yaml:"name"`
+//            Port       int    `yaml:"port"`
+//            TargetPort int    `yaml:"targetPort"`
+//            NodePort   int    `yaml:"nodePort,omitempty"`
+//        } `yaml:"ports"`
+//    } `yaml:"spec"`
+//}
+
 // Route - Struct to hold route information
 type Route struct {
 	Name        string
