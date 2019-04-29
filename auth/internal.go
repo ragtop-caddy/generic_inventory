7c154233dc8523b17e5fc45b5ea06227d3ed7a27
ragtop-caddy <ragtop_caddy@protonmail.com>
2019-04-29 14:16:44 -0700

    re-factored password set/get

--- auth/internal.go
+++ auth/internal.go
@@ -18,6 +18,11 @@ type InternalAuth struct {
 	Client *mongo.Database
 }
 
+type passHash struct {
+	Pass string `json:"pass,omitempty" bson:"pass,omitempty"`
+	Hash string `json:"hash,omitempty" bson:"hash,omitempty"`
+}
+
 // internalUser - Internally managed user
 type credentials struct {
 	UID        string    `json:"uid,omitempty" bson:"uid,omitempty"`
@@ -33,7 +38,20 @@ type credentials struct {
 	Last       time.Time `json:"last,omitempty" bson:"last,omitempty"`
 }
 
-type mcredentials []credentials
+// DisplayCredentials - struct to hold user information safe for display
+type DisplayCredentials struct {
+	UID        string    `json:"uid,omitempty" bson:"uid,omitempty"`
+	Fname      string    `json:"fname,omitempty" bson:"fname,omitempty"`
+	Lname      string    `json:"lname,omitempty" bson:"lname,omitempty"`
+	Email      string    `json:"email,omitempty" bson:"email,omitempty"`
+	Role       string    `json:"role,omitempty" bson:"role,omitempty"`
+	State      string    `json:"state,omitempty" bson:"state,omitempty"`
+	Inactive   int       `json:"inactive,omitempty" bson:"inactive,omitempty"`
+	Expiration int       `json:"expiration,omitempty" bson:"expiration,omitempty"`
+	Last       time.Time `json:"last,omitempty" bson:"last,omitempty"`
+}
+
+type mcredentials []DisplayCredentials
 
 // HashPassword - Create a hash of the provided password
 func HashPassword(password string) (string, error) {
@@ -47,29 +65,61 @@ func CheckPasswordHash(password, hash string) bool {
 	return err == nil
 }
 
-// GenDefaultUser - Function to generate a default user
-func (ia InternalAuth) GenDefaultUser() {
-	filter := bson.M{"uid": "Administrator"}
+// setPass - Function to update a user
+func (ia InternalAuth) setPass(uid, pass string) (res *mongo.UpdateResult, err error) {
+	hash, err := HashPassword(pass)
+	if err != nil {
+		log.Fatalf("ERROR: Unable to generate default password: %s", err)
+	}
+
+	filter := bson.M{"uid": uid}
+	update := bson.M{"$set": bson.M{"hash": hash}}
+	upsert := true
+	udopts := &options.UpdateOptions{Upsert: &upsert}
 	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
 	defer close()
+	res, err = ia.Client.Collection("users").UpdateOne(ctx, filter, update, udopts)
+	if err != nil {
+		log.Fatalf("ERROR: Unable to initialize the default account: %s", err)
+	}
+	return
+}
+
+func (ia InternalAuth) checkPass(uid, pass string) (ok bool, err error) {
+	var passhash passHash
+	filter := bson.M{"uid": uid}
+	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
+	defer close()
+	err = ia.Client.Collection("users").FindOne(ctx, filter).Decode(&passhash)
+	if err != nil {
+		log.Fatalf("ERROR: Unable to retrieve user password hash: %s", err)
+	}
+	ok = CheckPasswordHash(pass, passhash.Hash)
+	return
+}
+
+func genString() (s string) {
 	n := 12
 	b := make([]byte, n)
 	if _, err := rand.Read(b); err != nil {
 		panic(err)
 	}
-	s := fmt.Sprintf("%X", b)
+	s = fmt.Sprintf("%X", b)
 	fmt.Println(s)
-	passhash, err := HashPassword(s)
-	if err != nil {
-		log.Fatalf("ERROR: Unable to generate default password: %s", err)
-	}
+	return
+}
+
+// GenDefaultUser - Function to generate a default user
+func (ia InternalAuth) GenDefaultUser() {
+	filter := bson.M{"uid": "Administrator"}
+	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
+	defer close()
 	defaultUser := bson.M{
 		"$set": bson.M{
 			"uid":        "Administrator",
 			"fname":      "Inventory",
 			"lname":      "Administrator",
 			"email":      "administrator@localhost",
-			"hash":       passhash,
 			"role":       "admin",
 			"state":      "enabled",
 			"inactive":   60,
@@ -86,6 +136,13 @@ func (ia InternalAuth) GenDefaultUser() {
 		log.Fatalf("ERROR: Unable to initialize the default account: %s", err)
 	}
 	fmt.Printf("Matched %v documents and updated %v documents.\n", res.MatchedCount, res.ModifiedCount)
+
+	newPass := genString()
+	res, err = ia.setPass("Administrator", newPass)
+	if err != nil {
+		log.Fatalf("ERROR: Unable to update password: %s", err)
+	}
+	fmt.Printf("Matched %v documents and updated %v documents.\n", res.MatchedCount, res.ModifiedCount)
 }
 
 // Authenticate - Check password validity
@@ -96,18 +153,21 @@ func (ia InternalAuth) Authenticate(uid, pass string) (u User) {
 	if err != nil {
 		log.Printf("ERROR: %s while retrieving user credentials", err)
 	}
-	if CheckPasswordHash(pass, creds.Hash) {
+	check, err := ia.checkPass(uid, pass)
+	if err != nil {
+		log.Printf("ERROR: %s while checking user password", err)
+	}
+	if check {
 		u.Authenticated = true
 	}
 	u.Role = creds.Role
 	return u
 }
 
-func (ia InternalAuth) retrieveUser(uid string) (creds credentials, err error) {
+func (ia InternalAuth) retrieveUser(uid string) (creds DisplayCredentials, err error) {
 	filter := bson.M{"uid": uid}
 	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
 	defer close()
-	// need to pull hash out and return separately
 	err = ia.Client.Collection("users").FindOne(ctx, filter).Decode(&creds)
 	return
 }
@@ -121,7 +181,7 @@ func (ia InternalAuth) retrieveUsers() (results mcredentials, err error) {
 	defer c.Close(ctx)
 
 	for c.Next(ctx) {
-		var result credentials
+		var result DisplayCredentials
 		err = c.Decode(&result)
 		results = append(results, result)
 	}
