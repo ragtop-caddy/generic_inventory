e47d7cf586dc1f1cae6875c69e67b47d6bdc8f00
ragtop-caddy <ragtop_caddy@protonmail.com>
2019-05-01 12:36:42 -0700

    Removed unused cruft

--- auth/admin_handlers.go
+++ auth/admin_handlers.go
@@ -6,7 +6,6 @@ import (
 	"generic_inventory/web"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -126,67 +125,5 @@ func AdminCrudHandle(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(500) // Internal error
 			}
 		}
-		//	default:
-		//		if err := json.NewEncoder(w).Encode(r.URL.RawQuery); err != nil {
-		//			w.WriteHeader(500) // Internal error
-		//		}
 	}
-}
-
-// RetrieveUser - Return a JSON object containing the credentials for one user
-func RetrieveUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	session, err := Store.Get(r, "cookie-name")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	user := GetUser(session)
-	if user.Role != "admin" {
-		http.Redirect(w, r, "/", http.StatusFound)
-	}
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-	result, err := SessionAuth.retrieveUser(params["uid"])
-	if err != nil {
-		w.WriteHeader(404) // Not Found
-	} else {
-		if err := json.NewEncoder(w).Encode(result); err != nil {
-			w.WriteHeader(500) // Internal error
-		}
-	}
-}
-
-// RetrieveUsers - Return a JSON object containing the credentials for one user
-func RetrieveUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-	result, err := SessionAuth.retrieveUsers()
-	if err != nil {
-		w.WriteHeader(404) // Not Found
-	} else {
-		if err := json.NewEncoder(w).Encode(result); err != nil {
-			w.WriteHeader(500) // Internal error
-		}
-	}
-}
-
-// CreateUser - Creates a new user
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	var creds credentials
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-	if err != nil {
-		log.Printf("ERROR: %s", err)
-		return
-	}
-	err = json.Unmarshal(body, &creds)
-	if err != nil {
-		log.Printf("ERROR: %s", err)
-		return
-	}
-	result, err := SessionAuth.createUser(creds)
-	if err != nil {
-		log.Printf("ERROR: received %s while adding new user", err)
-	}
-	log.Printf("INFO: Got %s while creating new user %s", result, creds.UID)
 }
